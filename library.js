6b56e7bfe4e44f5f664904a850d98db04b65dd89
KenMR <kmr5591@gmail.com>
2022-12-01 19:50:36 -0500

    Include code to add information for newly created book objects to the table

--- library.js
+++ library.js
@@ -25,6 +25,25 @@ function Book(title, author, pages, readStatus) {
 function addBookToLibrary() {
     myLibrary.push(new Book(`${title.value}`, `${author.value}`, pages.value, `${readStatus.value}`));
     console.log(myLibrary);
+
+    let table = document.getElementById('list');
+    let row = table.insertRow(table.length);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+
+    cell1.innerHTML = `${title.value}`;
+    cell2.innerHTML = `${author.value}`;
+    cell3.innerHTML = `${pages.value}`;
+    cell4.innerHTML = `${readStatus.value}`;
+    cell5.innerHTML = `Delete`;
+
+    cell3.setAttribute('style', 'text-align: center');
+    cell4.setAttribute('style', 'text-align: center');
+    cell5.setAttribute('style', 'text-align: center');
+
 }
 
 myLibrary.push(new Book('The Lord of the Rings', 'J R R Tolkien', 1008, 'Read'));
