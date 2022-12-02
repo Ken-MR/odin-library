ea886ba6150f0c4471440cdf18ff19164f5456d3
KenMR <kmr5591@gmail.com>
2022-12-01 19:32:32 -0500

    Add function to take form entry and append new object to myLibrary array

--- library.js
+++ library.js
@@ -3,15 +3,15 @@ let unreadBooks = 0;
 let readBooks = 0;
 let bookTotal = 0;
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
+    this.readStatus = readStatus;
 
     this.info = function() {
         let read;
-        if (status === "Read") {
+        if (readStatus === "Read") {
             read = "has been read";
         }
         else {
@@ -20,6 +20,11 @@ function Book(title, author, pages, status) {
         return `${title} is a book by ${author}. It is ${pages} long and ${read}`
     };
 
+}
+
+function addBookToLibrary() {
+    myLibrary.push(new Book(`${title.value}`, `${author.value}`, pages.value, `${readStatus.value}`));
+    console.log(myLibrary);
 }
 
 myLibrary.push(new Book('The Lord of the Rings', 'J R R Tolkien', 1008, 'Read'));
