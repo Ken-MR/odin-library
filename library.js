702753b099c89d899592895f098f43436a8a3b05
KenMR <kmr5591@gmail.com>
2023-01-17 19:03:23 -0500

    Convert code to use class to generate objects

--- library.js
+++ library.js
@@ -1,30 +1,34 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 let myLibrary = [];
 let unreadBooks = 0;
 let readBooks = 0;
 let bookTotal = 0;
 
-function Book(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-    this.id = myLibrary.length;
+class Book {
+    constructor ({title, author, pages, readStatus}) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+        this.id = myLibrary.length;
+    }
 
-    this.info = function() {
+    info () {
         let read;
-        if (readStatus === "Read") {
+        if (this.readStatus === "Read") {
             read = "has been read";
         }
         else {
             read = "has not been read";
         }
-        return `${title} is a book by ${author}. It is ${pages} long and ${read}`
-    };
+        return `${this.title} is a book by ${this.author}. It is ${this.pages} long and ${read}`
+    }
 
 }
 
 function addBookToLibrary() {
-    myLibrary.push(new Book(`${title.value}`, `${author.value}`, pages.value, `${readStatus.value}`));
+    myLibrary.push(new Book({title: `${title.value}`, author: `${author.value}`, pages: `${pages.value}`, readStatus: `${readStatus.value}`}));
 
     let table = document.getElementById('list');
     let row = table.insertRow(table.length);
